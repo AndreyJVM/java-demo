e643194c1216f41d16667695011b05c7b0ea57ea
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-02 07:36:09 -0800

    Add new module, new class and test class

---
+++ realWorldJava/src/main/java/realWorldJava/analyzer/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,4 @@
+package realWorldJava;
+
+public class BankTransactionAnalyzerSimple {
+}
---
+++ realWorldJava/src/test/java/realWorldJava/analyzer/BankTransactionAnalyzerSimpleTest.java
@@ -0,0 +1,14 @@
+package realWorldJava.analyzer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankTransactionAnalyzerSimpleTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
