61008ac6a09b27249aaaf8a18755005445a91605
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-02 09:13:48 -0800

    add new class Main.class

---
+++ realWorldJava/src/main/java/realWorldJava/analyzer/Main.java
@@ -0,0 +1,50 @@
+package realWorldJava.analyzer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static final String RESOURCES = "src/main/resources";
+
+    public static void main(String[] args) throws IOException {
+        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+
+        final String fileName = args[0];
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions
+                = bankStatementCSVParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+
+        System.out.println("Transactions is January " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+
+
+    // Обработка списка транзакций
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
+        double total = 0d;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions,
+                                                      final Month month){
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                bankTransactionsInMonth.add(bankTransaction);
+            }
+        }
+        return bankTransactionsInMonth;
+    }
+}
