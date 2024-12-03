5ec7dcd7ef9262a08309049c4a6c953a78b1c8d0
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-03 03:55:26 -0800

    Add new test method in test class

--- realWorldJava/src/test/java/realWorldJava/analyzer/bank/StatementParserTest.java
+++ realWorldJava/src/test/java/realWorldJava/analyzer/bank/StatementParserTest.java
@@ -2,15 +2,29 @@ package realWorldJava.analyzer.bank;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class StatementParserTest {
 
-    private final StatementParser statementParser =
+    private final IStatementParser statementParser =
             new StatementParser();
 
     @Test
     public void shouldParseOneCorrectLineTest() throws Exception {
-        fail("Not yet implemented");
+        final String line = "30-01-2017,-50,Tesco";
+
+        final Transaction result = statementParser.parserFrom(line);
+
+        final Transaction expected
+                = new Transaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+        final double tolerance = 0.0d;
+
+        assertEquals(expected.getDate(), result.getDate());
+        assertEquals(expected.getAmount(), result.getAmount());
+        assertEquals(expected.getDescription(), result.getDescription());
     }
 }
