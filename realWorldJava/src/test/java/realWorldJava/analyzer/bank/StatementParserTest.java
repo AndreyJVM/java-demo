79d5b2ab4ea3eac5073c546a53c097ca8403e1e1
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-03 03:33:26 -0800

    Add new test StatementParserTest.java

---
+++ realWorldJava/src/test/java/realWorldJava/analyzer/bank/StatementParserTest.java
@@ -0,0 +1,16 @@
+package realWorldJava.analyzer.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class StatementParserTest {
+
+    private final StatementParser statementParser =
+            new StatementParser();
+
+    @Test
+    public void shouldParseOneCorrectLineTest() throws Exception {
+        fail("Not yet implemented");
+    }
+}
